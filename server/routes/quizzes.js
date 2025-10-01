b4caa8afaf9d2d731f588f4fa6676986a1e8cced
Aziz-BenLamine <azizbenlamine@gmail.com>
2025-10-01 11:20:36 +0100

    Update the AI model

--- server/routes/quizzes.js
+++ server/routes/quizzes.js
@@ -25,7 +25,7 @@ dotenv.config();
 
 // INIT GEMINI API
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
+const model = genAI.getGenerativeModel({ model: 'gemini-2.5-flash' });
 
 const generateQuestions = async (text) => {
   try {
